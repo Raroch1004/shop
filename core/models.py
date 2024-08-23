9ec83b16285b0118aee4118d51d53a909a3ff02f
Daniil <EaseJeat1999_1004@mail.ru>
2024-08-23 17:47:55 +0300

    Удалил лишнее, добавил verbose_name и related_name, сделал миграцию после изменений

--- core/models.py
+++ core/models.py
@@ -73,25 +73,31 @@ class Productimage(models.Model):
 
 class Cart(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    quantity = models.PositiveIntegerField(default=0)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата обновления')
 
     def __str__(self):
         return f'Cart {self.id} for {self.user}'
 
+    class Meta:
+        verbose_name = 'Корзина'
+        verbose_name_plural = 'Корзины'
+
 
 class CartItem(models.Model):
-    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    quantity = models.PositiveIntegerField(default=1)
+    cart = models.ForeignKey(Cart, related_name='items', on_delete=models.CASCADE, verbose_name='Корзина')
+    product = models.ForeignKey(Product, related_name='cart_items', on_delete=models.CASCADE, verbose_name='Товар')
+    quantity = models.PositiveIntegerField(default=1, verbose_name='Количество')
 
     def __str__(self):
         return f'{self.quantity} of {self.product.title} in Cart {self.cart.id}'
 
     def get_total_price(self):
         return self.quantity * self.product.price
+
+    class Meta:
+        verbose_name = 'Товар в Корзине'
+        verbose_name_plural = 'Товары в корзине'
 
 
 class Order(models.Model):
@@ -103,16 +109,17 @@ class Order(models.Model):
         ('cancelled', 'Cancelled'),  # Отменен
     ]
 
-    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
-    full_title = models.CharField(max_length=255)
-    email = models.EmailField()
-    address = models.CharField(max_length=255)
-    city = models.CharField(max_length=100)
-    postal_code = models.CharField(max_length=20)
-    country = models.CharField(max_length=50)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='pending')
+    user = models.ForeignKey(User, related_name='orders', on_delete=models.SET_NULL, null=True, blank=True,
+                             verbose_name='Пользователь')
+    full_title = models.CharField(max_length=255, verbose_name='ФИО')
+    email = models.EmailField(verbose_name='Email')
+    address = models.CharField(max_length=255, verbose_name='Адрес')
+    city = models.CharField(max_length=100, verbose_name='Город')
+    postal_code = models.CharField(max_length=20, verbose_name='Почтовый индекс')
+    country = models.CharField(max_length=50, verbose_name='Страна')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата обновления')
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='pending', verbose_name='Статус')
 
     def __str__(self):
         return f'Order {self.id} by {self.full_title}'
@@ -120,15 +127,23 @@ class Order(models.Model):
     def get_total_cost(self):
         return sum(item.get_total_price() for item in self.items.all())
 
+    class Meta:
+        verbose_name = 'Заказ'
+        verbose_name_plural = 'Заказы'
+
 
 class OrderItem(models.Model):
-    order = models.ForeignKey(Order, related_name='items', on_delete=models.CASCADE)
-    product = models.ForeignKey('Product', on_delete=models.CASCADE)
-    quantity = models.PositiveIntegerField(default=1)
-    price = models.DecimalField(max_digits=10, decimal_places=2)  # Цена на моментe заказа, чтобы зафиксировать цену
+    order = models.ForeignKey(Order, related_name='items', on_delete=models.CASCADE, verbose_name='Заказ')
+    product = models.ForeignKey('Product', related_name='order_items', on_delete=models.CASCADE, verbose_name='Товар')
+    quantity = models.PositiveIntegerField(default=1, verbose_name='Количество')
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена')
 
     def __str__(self):
         return f'{self.product.title} (x{self.quantity})'
 
     def get_total_price(self):
         return self.quantity * self.price
+
+    class Meta:
+        verbose_name = 'Товар в заказе'
+        verbose_name_plural = 'Товары в заказе'
