9c516b51af425de606b92d5848fde645fa7b182d
Daniil <EaseJeat1999_1004@mail.ru>
2024-08-29 18:53:59 +0300

    Изменил на МоделВьюсет

--- core/api/views.py
+++ core/api/views.py
@@ -1,9 +1,8 @@
 from django_filters import rest_framework as filters
-from rest_framework.response import Response
 
 from .filters import ProductFilter, CategoryFilter
 from .serializers import ProductSerializer, CategorySerializer, CartSerializer, CartItemSerializer
-from rest_framework import viewsets, status
+from rest_framework import viewsets
 from ..models import Product, Category, Cart, CartItem
 
 
@@ -23,56 +22,21 @@ class CategoryViewSet(viewsets.ReadOnlyModelViewSet):
     filterset_class = CategoryFilter
 
 
-class CartViewSet(viewsets.ViewSet):
+class CartViewSet(viewsets.ModelViewSet):
+    queryset = Cart.objects.all()
+    serializer_class = CartSerializer
 
-    def list(self, request):
-        queryset = Cart.objects.all()
-        serializer = CartSerializer(queryset, many=True)
-        return Response(serializer.data)
-
-    def retrieve(self, request, pk=None):
-        cart = Cart.objects.get(pk=pk, user=request.user)
-        serializer = CartSerializer(cart)
-        return Response(serializer.data)
+    def get_queryset(self):
+        return Cart.objects.filter(user=self.request.user)
 
 
-class CartItemViewSet(viewsets.ViewSet):
-    def list(self, request, cart_pk=None):
-        queryset = CartItem.objects.filter(cart__pk=cart_pk)
-        serializer = CartItemSerializer(queryset, many=True)
-        return Response(serializer.data)
+class CartItemViewSet(viewsets.ModelViewSet):
+    serializer_class = CartItemSerializer
 
-    def retrieve(self, request, pk=None, cart_pk=None):
-        try:
-            cart_item = CartItem.objects.get(pk=pk, cart__pk=cart_pk)
-        except CartItem.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)
-        serializer = CartItemSerializer(cart_item)
-        return Response(serializer.data)
+    def get_queryset(self):
+        cart_pk = self.kwargs.get('cart_pk')
+        return CartItem.objects.filter(cart__pk=cart_pk)
 
-    def create(self, request, cart_pk=None):
-        request.data['cart'] = cart_pk
-        serializer = CartItemSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def update(self, request, pk=None, cart_pk=None):
-        try:
-            cart_item = CartItem.objects.get(pk=pk, cart__pk=cart_pk)
-        except CartItem.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)
-        serializer = CartItemSerializer(cart_item, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def destroy(self, request, pk=None, cart_pk=None):
-        try:
-            cart_item = CartItem.objects.get(pk=pk, cart__pk=cart_pk)
-        except CartItem.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)
-        cart_item.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    def perform_create(self, serializer):
+        cart_pk = self.kwargs.get('cart_pk')
+        serializer.save(cart_id=cart_pk)
